110588637e3f90926c85dc25b83d1ebffcb60af8
NovikovaAnna <115411811+NovikovaAnna@users.noreply.github.com>
2022-10-26 20:08:26 +0300

    Add files via upload
    ЛР 4

---
+++ ЛР 4(2).py
@@ -0,0 +1,23 @@
+def get_count_char(str_):
+    str_ ="".join(sorted(str_.lower().split()))
+    count_char = {}
+    for char in str_:
+         if char.isalpha():
+             count_char[char] = (str_.count(char))
+
+    return count_char
+
+def take_percentage(count_char):
+    for n in count_char:
+        count_char[n] = round(count_char[n]/len(main_str)*100, 2)
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
+
+new_function = get_count_char(main_str)
+print(new_function)
+percentage = take_percentage(new_function)
+print(percentage)
